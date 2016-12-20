a7d49b2895b5322c6203188c5e330be1adcf7c9f
lusha <lusha90@163.com>
2016-12-20 10:25:07 +0800

    load log conf from classpath

--- driver/src/main/java/gw/com/cn/util/LogUtil.java
+++ driver/src/main/java/gw/com/cn/util/LogUtil.java
@@ -4,21 +4,55 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.File;
+import java.net.URL;
 
 /**
  * Created by lusha on 2016/11/29.
  */
+
 public class LogUtil {
 
-    private static Logger logger;
+    public static Logger getLogger(){
+        DzhLogUtil dzhLogUtil = new DzhLogUtil();
+        return dzhLogUtil.getLogger(true);
+    }
 
-    public static Logger getLogger() {
+}
+
+class DzhLogUtil {
+
+    private Logger logger;
+
+    private String logConfigPath;
+
+    public String getLogConfigPath() {
+        return logConfigPath;
+    }
+
+    public void setLogConfigPath(String logConfigPath) {
+        this.logConfigPath = logConfigPath;
+    }
+
+    public Logger getLogger(boolean isFromCP){
+        if(logger == null){
+            if (isFromCP){
+                initLoggerFromClassPath(this.getClass().getResource("/conf/dzhLog4j.properties"));
+            }else{
+                initLogger(logConfigPath);
+            }
+        }
         return logger;
     }
 
-    static{
-        File currentPath = new File("");
-        PropertyConfigurator.configure(currentPath.getAbsolutePath() + "\\src\\main\\resources\\conf\\dzhLog4j.properties");
+    public void initLoggerFromClassPath(URL configURL){
+        PropertyConfigurator.configure(configURL);
+        logger = Logger.getRootLogger();
+    }
+
+    public void initLogger(String logConfigPath){
+        File currentPath = new File(".");
+        //PropertyConfigurator.configure(currentPath.getAbsolutePath() + "\\src\\main\\resources\\conf\\dzhLog4j.properties");
+        PropertyConfigurator.configure(currentPath.getAbsolutePath() + logConfigPath);
         logger = Logger.getRootLogger();
     }
 
