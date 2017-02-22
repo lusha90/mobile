19ed734308e10468769f9483d9cc11bcacccb3e8
lusha <lusha90@163.com>
2017-02-22 14:04:20 +0800

    add cases

---
+++ android/src/main/java/gw/com/cn/testcase/selfSelection/DZH_TestCase_SelfSelection_028.java
@@ -0,0 +1,69 @@
+package gw.com.cn.testcase.selfSelection;
+
+import gw.com.cn.MarketAction;
+import gw.com.cn.SelfSelectionAction;
+import gw.com.cn.testcase.DZHBaseTestCase;
+import gw.com.cn.util.LogUtil;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class DZH_TestCase_SelfSelection_028 extends DZHBaseTestCase {
+
+    private SelfSelectionAction selfSelectionAction;
+    private MarketAction marketAction;
+
+    @BeforeMethod
+    public void setUp() {
+        super.setUp();
+        selfSelectionAction = new SelfSelectionAction("master");
+        marketAction = new MarketAction("master");
+        LogUtil.getLogger().info("1：null");
+        selfSelectionAction.skipAdv();
+        selfSelectionAction.deleteAllSelfStockAndLatestBrowse();
+    }
+    @Test(description = "大数量自选股和最新浏览(自选股和最新浏览各100个)")
+    public void testStep() {
+        super.testStep();
+        LogUtil.getLogger().info("1：进入市场页面，添加100个自选股");
+        marketAction.switchMarket();
+        marketAction.switchMainTab(MarketAction.MarkeMainTabItems.Stock);
+        marketAction.switchSubTab(MarketAction.MarkeTabItems.More);
+        marketAction.enterIntoSubItemViewOnMoreView("沪深A股");
+        marketAction.enterIntoStockDetailView();
+        int expect = marketAction.addSelfStockOnOnStockDetailView(this.getClass(), 100);
+        LogUtil.getLogger().info("2：点击编辑按钮，进入编辑自选股页面");
+        marketAction.back();
+        marketAction.back();
+        selfSelectionAction.enterIntoSelfStockView();
+        selfSelectionAction.enterIntoEditSelectionViewOnSelfSelectionView();
+        selfSelectionAction.editLatestBrowseOnEditSelectionView();
+        selfSelectionAction.toggleShowLatestBrowse(false);
+        selfSelectionAction.back();
+        int actual = selfSelectionAction.getAllSelfSelectionStock().size();
+        Assert.assertEquals(actual, expect ,"添加自选股数量一致");
+        LogUtil.getLogger().info("3：进入市场页面，添加100个股票");
+        marketAction.switchMarket();
+        marketAction.switchMainTab(MarketAction.MarkeMainTabItems.Stock);
+        marketAction.switchSubTab(MarketAction.MarkeTabItems.More);
+        marketAction.enterIntoSubItemViewOnMoreView("沪深A股");
+        marketAction.enterIntoStockDetailView();
+        int count = marketAction.addLatestBrowseOnStockDetailView(100);
+        LogUtil.getLogger().info("4：点击编辑按钮，进入编辑自选股页面");
+        marketAction.back();
+        marketAction.back();
+        selfSelectionAction.enterIntoSelfStockView();
+        selfSelectionAction.enterIntoEditSelectionViewOnSelfSelectionView();
+        selfSelectionAction.editLatestBrowseOnEditSelectionView();
+        selfSelectionAction.toggleShowLatestBrowse(true);
+        actual = selfSelectionAction.getAllLatestBrowseOnEditLatestBrowseView().size();
+        Assert.assertEquals(actual, count ,"最新浏览股票数量正确");
+        selfSelectionAction.back();
+    }
+    @AfterMethod
+    public void tearDown() {
+        super.tearDown();
+        selfSelectionAction.deleteAllSelfStockAndLatestBrowse();
+    }
+}
---
+++ android/src/main/java/gw/com/cn/testcase/selfSelection/DZH_TestCase_SelfSelection_029.java
@@ -0,0 +1,53 @@
+package gw.com.cn.testcase.selfSelection;
+
+import gw.com.cn.SelfSelectionAction;
+import gw.com.cn.testcase.DZHBaseTestCase;
+import gw.com.cn.util.LogUtil;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class DZH_TestCase_SelfSelection_029 extends DZHBaseTestCase {
+
+    private SelfSelectionAction selfSelectionAction;
+
+    @BeforeMethod
+    public void setUp() {
+        super.setUp();
+        selfSelectionAction = new SelfSelectionAction("master");
+        LogUtil.getLogger().info("1：删除所有自选股");
+        selfSelectionAction.skipAdv();
+        selfSelectionAction.deleteAllSelfStockAndLatestBrowse();
+    }
+
+    @Test(description = "断网时删除自选股")
+    public void testStep() {
+        super.testStep();
+        LogUtil.getLogger().info("1：进入自选股页面");
+        LogUtil.getLogger().info("2：点击搜索图标");
+        selfSelectionAction.enterIntoSearchStockViewOnSelfSelectionView();
+        LogUtil.getLogger().info("3：股票代码输入框输入555");
+        selfSelectionAction.typeTextOnSearchStockView("555");
+        LogUtil.getLogger().info("4：依次按顺序添加5个自选股");
+        List<String> list = selfSelectionAction.addStocksOnSearchStockView(5);
+        LogUtil.getLogger().info("5：返回到自选股页面");
+        selfSelectionAction.back();
+        selfSelectionAction.back();
+        LogUtil.getLogger().info("6：删除一个自选股");
+        selfSelectionAction.closeWifi();
+        Assert.assertEquals(selfSelectionAction.networkIsAvailable(), false, "网络连接已经断开");
+        selfSelectionAction.selfStockOperatorOnSelectionView(list.get(0), SelfSelectionAction.StockOperator.DEL, false);
+        selfSelectionAction.checkExistSpecialSelfStockOnEditSelectionViewOrSelectionView(list.get(0));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        super.tearDown();
+        selfSelectionAction.openWifi();
+        selfSelectionAction.back();
+        selfSelectionAction.deleteAllSelfStockAndLatestBrowse();
+    }
+}
